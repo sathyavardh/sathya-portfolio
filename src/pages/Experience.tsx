0b88db4fdef64941d376117300d79ff6207a65df
sathyavardhan-k <sathyavardhan.k@gwcdata.ai>
2025-06-02 09:54:28 +0530

    fixed exp

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -66,7 +66,7 @@ const Experience = () => {
                       </div>
                       <div className="flex items-center text-gray-600 dark:text-gray-400">
                         <MapPin size={18} className="mr-2" />
-                        <span>Erode, TamilNadu, India</span>
+                        <span>Salem, TamilNadu, India</span>
                       </div>
                     </div>
 
